9606b9a73495f8c28de77e6023b8fb441fe66c54
Edward Minnett <edward@ejmdesign.co.uk>
2015-04-08 21:05:54 +0100

    Added a stub for the shuffler service.

---
+++ app/services/shuffler.rb
@@ -0,0 +1,40 @@
+##
+# This class encapsulates the various methods of shuffling cards and requisite
+# logic associated with those methods. The various methods present were heavily
+# inspired by the Wikipedia page on shuffling cards (http://en.wikipedia.org/wiki/Shuffling).
+
+class Shuffler
+
+  def self.shuffle(stack)
+    self.fisher_yates_shuffle(stack)
+  end
+
+  def self.cut(stack)
+
+  end
+
+  def self.overhand_shuffle(stack)
+
+  end
+
+  def self.rifle_shuffle(stack)
+
+  end
+
+  def self.pile_shuffle(stack)
+
+  end
+
+  def self.mongean_shuffle(stack)
+
+  end
+
+  def self.mexican_spiral_shuffle(stack)
+
+  end
+
+  def self.fisher_yates_shuffle(stack)
+
+  end
+
+end
